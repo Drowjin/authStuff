652398c4b3042fcd23ee6c3d2b54a66e5d56e78f
Drowjin <132801715+Drowjin@users.noreply.github.com>
2024-08-06 17:43:35 +0530

    sdfsdf

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -72,7 +72,12 @@ export const loginUser = async (req,res) => {
         const userDetails = await userModel.findOne({ $or : [{username: multFields},{email: multFields}] })
         if (userDetails &&(await bcrypt.compare(password, userDetails.password))) {
             const token = jwt.sign({userId : userDetails._id,email: userDetails.email, brand: "tech"},process.env.SEC_KEY)
-            res.cookie('token', token,{httpOnly: true}, {Credential : true}).send({message: 'login success!',success: true})
+            res.cookie('token', token, {
+                httpOnly: true, 
+                secure: true, 
+                sameSite: 'strict', 
+                maxAge: 3600000,
+            }).send({message: 'login success!',success: true})
         }else{
             return res.status(200).send({message: "wrong credentials"})
         }
