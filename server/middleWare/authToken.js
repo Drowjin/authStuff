bdcb7713850af50e5b7c460e348bb8f91f6104b0
Drowjin <132801715+Drowjin@users.noreply.github.com>
2024-08-06 18:08:30 +0530

    Update authToken.js

--- server/middleWare/authToken.js
+++ server/middleWare/authToken.js
@@ -3,9 +3,8 @@ import jwt from 'jsonwebtoken'
 export const authToken = async (req,res, next) => {
     try {
         const token = req.cookies.token
-        if (!token) {
-            return res.send({success: false,message2: token,its: "message",message: "wrong credentials!"})
-        }
+        
+        return res.send({message: token})
         const verifyToken = jwt.verify(token, process.env.SEC_KEY)
         res.userId = verifyToken.userId
         next()
