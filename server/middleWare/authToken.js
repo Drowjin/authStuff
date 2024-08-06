43937b716aeeb144cd91150bfae89fffb0861dcd
katariya dhruv <drowjin007@gmail.com>
2024-08-06 22:27:41 +0530

    sssssssss

--- server/middleWare/authToken.js
+++ server/middleWare/authToken.js
@@ -4,7 +4,7 @@ export const authToken = async (req, res, next) => {
     try {
         const token = req.cookies.token
         if (!token) {
-            res.send({ success: false, message: "token is not defind!" })
+            res.send({ success: false, message: "token is not defind!",cookies: req.cookies })
         }
         const verifyToken = jwt.verify(token, process.env.SEC_KEY)
         res.userId = verifyToken.userId
